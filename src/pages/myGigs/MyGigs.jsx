66d3d1a6948f5e68de56243ce8ebcee2dcbc9af5
Vivek <vivekraj5217@gmail.com>
2023-06-13 12:21:09 +0530

    Added MyGigs page

--- src/pages/myGigs/MyGigs.jsx
+++ src/pages/myGigs/MyGigs.jsx
@@ -1,10 +1,113 @@
-import React from 'react'
-import "./MyGigs.scss"
+import React from "react";
+import { Link } from "react-router-dom";
+import "./MyGigs.scss";
 
 const MyGigs = () => {
   return (
-    <div className='mygigs'>MyGigs</div>
-  )
-}
+    <div className="myGigs">
+      <div className="container">
+        <div className="title">
+          <h1>Gigs</h1>
+          <Link className="link" to="/add">
+            <button>Add New Gig</button>
+          </Link>
+        </div>
+        <table>
+          <tr>
+            <th>Image</th>
+            <th>Title</th>
+            <th>Price</th>
+            <th>Sales</th>
+            <th>Action</th>
+          </tr>
+          <tr>
+            <td>
+              <img className="gigImg"
+                src="https://fiverr-res.cloudinary.com/image/upload/t_portfolio_project_large,q_auto,f_auto/v1/attachments/project_item/attachment/e15b529f01e9656bdf634bdb3f96c164-1684515299617/animal%20to%20character%2002.png"
+                alt=""
+              />
+            </td>
+            <td>Gig01</td>
+            <td>123</td>
+            <td>12</td>
+            <td>
+              <img className="deleteImg" src="/img/delete.png" alt="" />
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <img className="gigImg"
+                src="https://fiverr-res.cloudinary.com/image/upload/t_portfolio_project_large,q_auto,f_auto/v1/attachments/project_item/attachment/e15b529f01e9656bdf634bdb3f96c164-1684515299617/animal%20to%20character%2002.png"
+                alt=""
+              />
+            </td>
+            <td>Gig01</td>
+            <td>123</td>
+            <td>12</td>
+            <td>
+              <img className="deleteImg" src="/img/delete.png" alt="" />
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <img className="gigImg"
+                src="https://fiverr-res.cloudinary.com/image/upload/t_portfolio_project_large,q_auto,f_auto/v1/attachments/project_item/attachment/e15b529f01e9656bdf634bdb3f96c164-1684515299617/animal%20to%20character%2002.png"
+                alt=""
+              />
+            </td>
+            <td>Gig01</td>
+            <td>123</td>
+            <td>12</td>
+            <td>
+              <img className="deleteImg" src="/img/delete.png" alt="" />
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <img className="gigImg"
+                src="https://fiverr-res.cloudinary.com/image/upload/t_portfolio_project_large,q_auto,f_auto/v1/attachments/project_item/attachment/e15b529f01e9656bdf634bdb3f96c164-1684515299617/animal%20to%20character%2002.png"
+                alt=""
+              />
+            </td>
+            <td>Gig01</td>
+            <td>123</td>
+            <td>12</td>
+            <td>
+              <img className="deleteImg" src="/img/delete.png" alt="" />
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <img className="gigImg"
+                src="https://fiverr-res.cloudinary.com/image/upload/t_portfolio_project_large,q_auto,f_auto/v1/attachments/project_item/attachment/e15b529f01e9656bdf634bdb3f96c164-1684515299617/animal%20to%20character%2002.png"
+                alt=""
+              />
+            </td>
+            <td>Gig01</td>
+            <td>123</td>
+            <td>12</td>
+            <td>
+              <img className="deleteImg" src="/img/delete.png" alt="" />
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <img className="gigImg"
+                src="https://fiverr-res.cloudinary.com/image/upload/t_portfolio_project_large,q_auto,f_auto/v1/attachments/project_item/attachment/e15b529f01e9656bdf634bdb3f96c164-1684515299617/animal%20to%20character%2002.png"
+                alt=""
+              />
+            </td>
+            <td>Gig01</td>
+            <td>123</td>
+            <td>12</td>
+            <td>
+              <img className="deleteImg" src="/img/delete.png" alt="" />
+            </td>
+          </tr>
+        </table>
+      </div>
+    </div>
+  );
+};
 
-export default MyGigs
+export default MyGigs;
