3f25b478bf6dbf8ea2d713c0df099a3f1eb07f13
Vivek <vivekraj5217@gmail.com>
2023-07-12 13:46:15 +0530

    Login implementation

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,10 +1,51 @@
-import React from 'react'
-import "./Login.scss"
+import React, { useState } from "react";
+import "./Login.scss";
+import newRequest from "../../utils/newReques";
+import { useNavigate } from "react-router-dom";
 
 const Login = () => {
-  return (
-    <div className='login'>Login</div>
-  )
-}
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState(null);
 
-export default Login
+  const navigate = useNavigate();
+
+  const handleSubmit = async (event) => {
+    event.preventDefault(); //To not submit page
+
+    try {
+      const res = await newRequest.post("auth/login", { username, password });
+      //Storing user data in local storage to use it at different pages/components like navbar
+      localStorage.setItem("currentUser", JSON.stringify(res.data)); // we can and should use Context API here. Not used to keep is simple for now
+      navigate("/");
+    } catch (error) {
+      setError(error.response.data);
+    }
+  };
+
+  return (
+    <div className="login" onSubmit={handleSubmit}>
+      <div className="container">
+        <form action="">
+          <h1>Sign In</h1>
+          <label htmlFor="">Username</label>
+          <input
+            type="text"
+            name="username"
+            onChange={(event) => setUsername(event.target.value)}
+          />
+          <label htmlFor="">Password</label>
+          <input
+            type="text"
+            name="password"
+            onChange={(event) => setPassword(event.target.value)}
+          />
+          <button type="submit">Login</button>
+          {error || error}
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
