91de9a0147e6523ab9d867924f08ebba8d84ae54
Vivek <vivekraj5217@gmail.com>
2023-08-03 14:51:09 +0530

    Added search feature

--- src/components/featured/Featured.jsx
+++ src/components/featured/Featured.jsx
@@ -1,19 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Featured.scss";
+import { useNavigate } from "react-router-dom";
 
 const Featured = () => {
+  const [input, setInput] = useState("");
+
+  const navigate = useNavigate();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    navigate(`/gigs?search=${input}`);
+  };
+
   return (
     <div className="featured">
       <div className="container">
         <div className="left">
-          <h1>Find the perfect <i>freelance</i> service for your business</h1>
-          <div className="search">
+          <h1>
+            Find the perfect <i>freelance</i> service for your business
+          </h1>
+          <form className="search" onSubmit={(e) => handleSubmit(e)}>
             <div className="searchInput">
               <img className="searchImage" src="./img/search.png" alt="" />
-              <input type="text" placeholder='Try "building mobile app"' />
+              <input
+                type="text"
+                placeholder='Try "building mobile app"'
+                value={input}
+                onChange={(e) => {
+                  setInput(e.target.value);
+                }}
+              />
             </div>
-            <button>Search</button>
-          </div>
+            <button type="submit">Search</button>
+          </form>
           <div className="popular">
             <span>Popular:</span>
             <button>Web Design</button>
