588d2e19174a13c3475af6bf989281d18b097920
Vivek <vivekraj5217@gmail.com>
2023-08-13 15:31:50 +0530

    Added addGig and myGigs page functionality

--- src/pages/addGig/AddGig.jsx
+++ src/pages/addGig/AddGig.jsx
@@ -1,7 +1,73 @@
-import React from "react";
+import React, { useReducer, useState } from "react";
 import "./AddGig.scss";
+import { INITIAL_STATE, gigReducer } from "../../reducers/gigReducer";
+import upload from "../../utils/upload";
+import { useNavigate } from "react-router-dom";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import newRequest from "../../utils/newRequest";
 
 const AddGig = () => {
+  const [singlefile, setSingleFile] = useState(undefined);
+  const [files, setFiles] = useState([]);
+  const [uploading, setUploading] = useState(false);
+
+  const [state, dispatch] = useReducer(gigReducer, INITIAL_STATE);
+
+  const handleChange = (event) => {
+    dispatch({
+      type: "CHANGE_INPUT",
+      payload: { name: event.target.name, value: event.target.value },
+    });
+  };
+
+  const handleFeature = (event) => {
+    event.preventDefault();
+    dispatch({
+      type: "ADD_FEATURE",
+      payload: event.target[0].value,
+    });
+    event.target[0].value = "";
+  };
+
+  const handleUpload = async () => {
+    setUploading(true);
+    try {
+      const cover = await upload(singlefile);
+
+      const images = await Promise.all(
+        // files is a FileList object. [...files] converts it to normal js file array.
+        [...files].map(async (file) => {
+          const url = await upload(file);
+          return url;
+        })
+      );
+      setUploading(false);
+      dispatch({ type: "ADD_IMAGES", payload: { cover, images } });
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  console.log(state);
+
+  const navigate = useNavigate();
+  const queryClient = useQueryClient();
+
+  const mutation = useMutation({
+    mutationFn: (gig) => {
+      return newRequest.post("/gigs", gig);
+    },
+    onSuccess: () => {
+      queryClient.invalidateQueries(["myGigs"]);
+    },
+  });
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    mutation.mutate(state);
+    // navigate("/mygigs");
+  };
+
   return (
     <div className="addGig">
       <div className="container">
@@ -9,38 +75,103 @@ const AddGig = () => {
         <div className="sections">
           <div className="left">
             <label htmlFor="">Title</label>
-            <input type="text" name="" id="" placeholder="e.g. I will do something I am really good at" />
+            <input
+              type="text"
+              name="title"
+              placeholder="e.g. I will do something I am really good at"
+              onChange={handleChange}
+            />
             <label htmlFor="">Category</label>
-            <select name="categories" id="cats">
+            <select name="cat" id="cat" onChange={handleChange}>
               <option value="design">Design</option>
               <option value="web">Web Development</option>
               <option value="animation">Animation</option>
               <option value="music">Music</option>
             </select>
-            <label htmlFor="">Cover Images</label>
-            <input type="file" name="" id="" />
-            <label htmlFor="">Upload Images</label>
-            <input type="file" name="" id="" multiple />
+
+            <div className="images">
+              <div className="imagesInput">
+                <label htmlFor="">Cover Images</label>
+                <input
+                  type="file"
+                  name=""
+                  id=""
+                  onChange={(e) => setSingleFile(e.target.files[0])}
+                />
+                <label htmlFor="">Upload Images</label>
+                <input
+                  type="file"
+                  name=""
+                  id=""
+                  multiple
+                  onChange={(e) => setFiles(e.target.files)}
+                />
+              </div>
+              <button onClick={handleUpload}>
+                {uploading ? "uploading" : "Upload"}
+              </button>
+            </div>
+
             <label htmlFor="">Description</label>
-            <textarea name="" id="" cols="30" rows="16" placeholder="Brief description to introduce your service"></textarea>
-            <button>Create</button>
+            <textarea
+              name="desc"
+              cols="30"
+              rows="16"
+              placeholder="Brief description to introduce your service"
+              onChange={handleChange}
+            ></textarea>
+            <button onClick={handleSubmit}>Create</button>
           </div>
           <div className="right">
             <label htmlFor="">Service Title</label>
-            <input type="text" name="" id="" placeholder="e.g. One-page web design" />
+            <input
+              type="text"
+              name="shortTitle"
+              placeholder="e.g. One-page web design"
+              onChange={handleChange}
+            />
             <label htmlFor="">Short Description</label>
-            <textarea name="" id="" cols="30" rows="10" placeholder="Short description for your service"></textarea>
+            <textarea
+              name="shortDesc"
+              cols="30"
+              rows="10"
+              placeholder="Short description for your service"
+              onChange={handleChange}
+            ></textarea>
             <label htmlFor="">Delivery Time (e.g. 3 days)</label>
-            <input type="number" name="" id="" min={1} />
+            <input
+              type="number"
+              name="deliveryTime"
+              min={1}
+              onChange={handleChange}
+            />
             <label htmlFor="">Revision Number</label>
-            <input type="number" name="" id="" min={1} />
+            <input
+              type="number"
+              name="revisionNumber"
+              min={1}
+              onChange={handleChange}
+            />
             <label htmlFor="">Add Features</label>
-            <input type="text" name="" id="e.g. page design" />
-            <input type="text" name="" id="e.g. file uploading" />
-            <input type="text" name="" id="e.g. setting up a domain" />
-            <input type="text" name="" id="e.g. hosting" />
+            <form action="" className="addFeature" onSubmit={handleFeature}>
+              <input type="text" name="" placeholder="e.g. page design" />
+              <button type="submit">Add</button>
+            </form>
+            <div className="addedFeatures">
+              {state?.features?.map((feature) => (
+                <button
+                  key={feature}
+                  onClick={() =>
+                    dispatch({ type: "REMOVE_FEATURE", payload: feature })
+                  }
+                >
+                  {feature}
+                  <span> X</span>
+                </button>
+              ))}
+            </div>
             <label htmlFor="">Price</label>
-            <input type="number" name="" id="" min={1} />
+            <input type="number" name="price" min={1} onChange={handleChange} />
           </div>
         </div>
       </div>
--- src/pages/myGigs/MyGigs.jsx
+++ src/pages/myGigs/MyGigs.jsx
@@ -1,111 +1,83 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "./MyGigs.scss";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+import newRequest from "../../utils/newRequest";
 
 const MyGigs = () => {
+  const currentUser = JSON.parse(localStorage.getItem("currentUser"));
+
+  const queryClient = useQueryClient();
+
+  const { isLoading, data, error } = useQuery({
+    queryKey: ["myGigs"],
+    queryFn: () => {
+      return newRequest.get(`/gigs?userId=${currentUser._id}`).then((res) => {
+        return res.data;
+      });
+    },
+  });
+
+  const mutation = useMutation({
+    mutationFn: (id) => {
+      return newRequest.delete(`/gigs/${id}`);
+    },
+    onSuccess: () => {
+      queryClient.invalidateQueries(["myGigs"]);
+    },
+  });
+
+  const handleDelete = (id) => {
+    mutation.mutate(id);
+  };
+
   return (
     <div className="myGigs">
-      <div className="container">
-        <div className="title">
-          <h1>Gigs</h1>
-          <Link className="link" to="/addGig">
-            <button>Add New Gig</button>
-          </Link>
+      {isLoading ? (
+        "Loading"
+      ) : error ? (
+        "Something went wrong"
+      ) : (
+        <div className="container">
+          <div className="title">
+            <h1>Gigs</h1>
+            <Link className="link" to="/addGig">
+              <button>Add New Gig</button>
+            </Link>
+          </div>
+          <table>
+            <tr>
+              <th>Image</th>
+              <th>Title</th>
+              <th>Price</th>
+              <th>Sales</th>
+              <th>Action</th>
+            </tr>
+            {data.map((gig) => (
+              <tr key={gig._id}>
+                <td>
+                  <img className="gigImg" src={gig.cover} alt="" />
+                </td>
+                <td>
+                  <Link to={`/gig/${gig._id}`} className="link">
+                    {gig.title}
+                  </Link>
+                </td>
+                <td>{gig.price}</td>
+                <td>{gig.sales}</td>
+                <td>
+                  <img
+                    className="deleteImg"
+                    src="/img/delete.png"
+                    alt=""
+                    onClick={() => handleDelete(gig.id)}
+                  />
+                </td>
+              </tr>
+            ))}
+          </table>
         </div>
-        <table>
-          <tr>
-            <th>Image</th>
-            <th>Title</th>
-            <th>Price</th>
-            <th>Sales</th>
-            <th>Action</th>
-          </tr>
-          <tr>
-            <td>
-              <img className="gigImg"
-                src="https://fiverr-res.cloudinary.com/image/upload/t_portfolio_project_large,q_auto,f_auto/v1/attachments/project_item/attachment/e15b529f01e9656bdf634bdb3f96c164-1684515299617/animal%20to%20character%2002.png"
-                alt=""
-              />
-            </td>
-            <td>Gig01</td>
-            <td>123</td>
-            <td>12</td>
-            <td>
-              <img className="deleteImg" src="/img/delete.png" alt="" />
-            </td>
-          </tr>
-          <tr>
-            <td>
-              <img className="gigImg"
-                src="https://fiverr-res.cloudinary.com/image/upload/t_portfolio_project_large,q_auto,f_auto/v1/attachments/project_item/attachment/e15b529f01e9656bdf634bdb3f96c164-1684515299617/animal%20to%20character%2002.png"
-                alt=""
-              />
-            </td>
-            <td>Gig01</td>
-            <td>123</td>
-            <td>12</td>
-            <td>
-              <img className="deleteImg" src="/img/delete.png" alt="" />
-            </td>
-          </tr>
-          <tr>
-            <td>
-              <img className="gigImg"
-                src="https://fiverr-res.cloudinary.com/image/upload/t_portfolio_project_large,q_auto,f_auto/v1/attachments/project_item/attachment/e15b529f01e9656bdf634bdb3f96c164-1684515299617/animal%20to%20character%2002.png"
-                alt=""
-              />
-            </td>
-            <td>Gig01</td>
-            <td>123</td>
-            <td>12</td>
-            <td>
-              <img className="deleteImg" src="/img/delete.png" alt="" />
-            </td>
-          </tr>
-          <tr>
-            <td>
-              <img className="gigImg"
-                src="https://fiverr-res.cloudinary.com/image/upload/t_portfolio_project_large,q_auto,f_auto/v1/attachments/project_item/attachment/e15b529f01e9656bdf634bdb3f96c164-1684515299617/animal%20to%20character%2002.png"
-                alt=""
-              />
-            </td>
-            <td>Gig01</td>
-            <td>123</td>
-            <td>12</td>
-            <td>
-              <img className="deleteImg" src="/img/delete.png" alt="" />
-            </td>
-          </tr>
-          <tr>
-            <td>
-              <img className="gigImg"
-                src="https://fiverr-res.cloudinary.com/image/upload/t_portfolio_project_large,q_auto,f_auto/v1/attachments/project_item/attachment/e15b529f01e9656bdf634bdb3f96c164-1684515299617/animal%20to%20character%2002.png"
-                alt=""
-              />
-            </td>
-            <td>Gig01</td>
-            <td>123</td>
-            <td>12</td>
-            <td>
-              <img className="deleteImg" src="/img/delete.png" alt="" />
-            </td>
-          </tr>
-          <tr>
-            <td>
-              <img className="gigImg"
-                src="https://fiverr-res.cloudinary.com/image/upload/t_portfolio_project_large,q_auto,f_auto/v1/attachments/project_item/attachment/e15b529f01e9656bdf634bdb3f96c164-1684515299617/animal%20to%20character%2002.png"
-                alt=""
-              />
-            </td>
-            <td>Gig01</td>
-            <td>123</td>
-            <td>12</td>
-            <td>
-              <img className="deleteImg" src="/img/delete.png" alt="" />
-            </td>
-          </tr>
-        </table>
-      </div>
+      )}
     </div>
   );
 };
