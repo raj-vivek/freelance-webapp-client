5d8226d273d24faaa9951af1b24bdb6e4e4afc80
Vivek <vivekraj5217@gmail.com>
2023-08-29 19:28:51 +0530

    Added link to Project Cards

--- src/components/projectCard/ProjectCard.jsx
+++ src/components/projectCard/ProjectCard.jsx
@@ -3,9 +3,9 @@ import "./ProjectCard.scss";
 import { PropTypes } from "prop-types";
 import { useQuery } from "@tanstack/react-query";
 import newRequest from "../../utils/newRequest";
+import { Link } from "react-router-dom";
 
 const ProjectCard = ({ gig }) => {
-
   const { isLoading, error, data } = useQuery({
     queryKey: ["gigUser", gig.userId],
     queryFn: () => {
@@ -17,20 +17,22 @@ const ProjectCard = ({ gig }) => {
 
   return (
     <div className="projectCard">
-      <img className="mainImg" src={gig.cover} />
-      {isLoading ? (
-        "Loading"
-      ) : error ? (
-        "Something went wrong"
-      ) : (
-        <div className="info">
-          <img src={data.img} alt="" />
-          <div className="textInfo">
-            <h4>{gig.shortTitle}</h4>
-            <span>by {data.username}</span>
+      <Link className="link projectCardLink" to={`/gig/${gig._id}`}>
+        <img className="mainImg" src={gig.cover} />
+        {isLoading ? (
+          "Loading"
+        ) : error ? (
+          "Something went wrong"
+        ) : (
+          <div className="info">
+            <img src={data.img} alt="" />
+            <div className="textInfo">
+              <h4>{gig.shortTitle}</h4>
+              <span>by {data.username}</span>
+            </div>
           </div>
-        </div>
-      )}
+        )}
+      </Link>
     </div>
   );
 };
